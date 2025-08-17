a472d5ab689cbe35d5ae1daf20085341594ea702
siro53 <shusuke.shirotake@me.com>
2025-08-17 13:34:54 +0900

    feat: dsu のテストを追加

---
+++ verify/src/ds/dsu.rs
@@ -0,0 +1,25 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use ds::dsu::Dsu;
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize
+    }
+    let mut uf = Dsu::new(n);
+    for _ in 0..q {
+        input! {
+            t: usize,
+            u: usize,
+            v: usize
+        };
+        if t == 0 {
+            uf.merge(u, v);
+        } else {
+            let res = if uf.same(u, v) { 1 } else { 0 };
+            println!("{}", res);
+        }
+    }
+}
